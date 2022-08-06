52bab5eb80bf550a1e236d852cabba26b2e085ac
Ken-mbira <devmbira@gmail.com>
2022-08-06 12:12:57 +0300

    add check to make sure file is of type csv

---
+++ helpers.py
@@ -0,0 +1,7 @@
+import pathlib
+
+def is_csv(file_path:str) -> bool:
+    if pathlib.Path(file_path).suffix == '.csv':
+        return True
+    
+    return False
--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 import csv
+from datetime import datetime
+import os
+
+import helpers
 
 def main():
-    with open('staging/July17.csv',mode='r') as my_file:
-        for row in my_file:
-            print(row)
+    todays_date = datetime.today().strftime('%d %B %Y')
 
-if __name__ == '__main__':
+    try:
+        for file_name in os.scandir('staging'):
+            if helpers.is_csv(file_name):
+                print(file_name)
+
+    except FileNotFoundError as e:
+        print(e.strerror)
+    # with open('staging/July17.csv',mode='r') as my_file:
+    #     for row in my_file:
+    #         print(row)
+
+if __name__ == '__main__':  
     main()
